71861b1905cd48d976ffdd3d0b07f27c6c47f8ba
Olesia Erastova <olesia1205@yandex.ru>
2024-07-26 15:14:09 +0300

    add webLarekAPI

---
+++ src/components/WebLarekAPI.ts
@@ -0,0 +1,31 @@
+import { IOrder, IOrderData, IProduct, IWebLarekAPI } from '../types';
+import { Api, ApiListResponse } from './base/api';
+
+export class WebLarekAPI extends Api implements IWebLarekAPI {
+	readonly cdn: string;
+
+	constructor(cdn: string, baseUrl: string, options?: RequestInit) {
+		super(baseUrl, options);
+		this.cdn = cdn;
+	};
+
+	getProducts(): Promise<IProduct[]> {
+		return this.get('/product').then((data: ApiListResponse<IProduct>) =>
+			data.items.map((item) => ({
+				...item,
+				image: this.cdn + item.image,
+			}))
+		);
+	};
+
+	getProductById(id: string): Promise<IProduct> {
+		return this.get(`product/${id}`).then((item: IProduct) => ({
+			...item,
+			image: this.cdn + item.image,
+		}));
+	};
+
+	postOrder(order: IOrder): Promise<IOrderData> {
+		return this.post(`/order`, order).then((data: IOrderData) => data);
+	};
+};
