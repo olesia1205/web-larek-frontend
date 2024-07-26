15cb6f60696042a96b9279da1fe1ea4c2ff04c70
Olesia Erastova <olesia1205@yandex.ru>
2024-07-26 15:19:24 +0300

    add component Page

---
+++ src/components/Page.ts
@@ -0,0 +1,39 @@
+import { IEvents, IPage } from '../types';
+import { ensureElement } from '../utils/utils';
+import { Component } from './base/Component';
+
+export class Page extends Component<IPage> {
+	protected _catalog: HTMLElement;
+	protected _basket: HTMLElement;
+	protected _counter: HTMLElement;
+	protected _wrapper: HTMLElement;
+
+	constructor(container: HTMLElement, protected events: IEvents) {
+		super(container);
+
+		this._catalog = ensureElement<HTMLElement>('.gallery');
+		this._basket = ensureElement<HTMLElement>('.header__basket');
+		this._counter = ensureElement<HTMLElement>('.header__basket-counter');
+		this._wrapper = ensureElement<HTMLElement>('.page__wrapper');
+
+		this._basket.addEventListener('click', () => {
+			this.events.emit('basket:open');
+		});
+	};
+
+	set catalog(products: HTMLElement[]) {
+		this._catalog.replaceChildren(...products);
+	};
+
+	set counter(value: number) {
+		this.setText(this._counter, String(value));
+	};
+
+	set locked(value: boolean) {
+		if (value) {
+			this.toggleClass(this._wrapper, 'page__wrapper_locked', true);
+		} else {
+			this.toggleClass(this._wrapper, 'page__wrapper_locked', false);
+		};
+	};
+};
