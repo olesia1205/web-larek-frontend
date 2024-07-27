302f94bb437732b4327203120797c74d3672f4c2
Olesia Erastova <olesia1205@yandex.ru>
2024-07-27 15:40:21 +0300

    add component Modal & open cardPreview logic

---
+++ src/components/Modal.ts
@@ -0,0 +1,42 @@
+import { IEvents, IModalData } from '../types';
+import { ensureElement } from '../utils/utils';
+import { Component } from './base/Component';
+
+export class Modal extends Component<IModalData> {
+	protected _content: HTMLElement;
+	protected _closeButton: HTMLButtonElement;
+
+	constructor(container: HTMLElement, protected events: IEvents) {
+		super(container);
+
+		this._content = ensureElement<HTMLElement>('.modal__content', container);
+		this._closeButton = ensureElement<HTMLButtonElement>(
+			'.modal__close',
+			container
+		);
+
+		this._content.addEventListener('click', (event) => event.stopPropagation());
+		this._closeButton.addEventListener('click', this.closeModal.bind(this));
+		this.container.addEventListener('click', this.closeModal.bind(this));
+	};
+
+	openModal() {
+		this.toggleClass(this.container, 'modal_active', true);
+		this.events.emit('modal:open');
+	};
+
+	closeModal() {
+		this.toggleClass(this.container, 'modal_active', false);
+		this.events.emit('modal:close');
+	};
+
+	render(data: IModalData): HTMLElement {
+		super.render(data);
+		this.openModal();
+		return this.container;
+	};
+
+	set content(value: HTMLElement) {
+		this._content.replaceChildren(value);
+	};
+};
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import './scss/styles.scss';
 import { AppState } from './components/AppState';
 import { EventEmitter } from './components/base/EventEmitter';
 import { Page } from './components/Page';
+import { Modal } from './components/Modal';
 import { Card } from './components/Card';
 import { WebLarekAPI } from './components/WebLarekAPI';
 import { IProduct } from './types';
@@ -14,8 +15,10 @@ const appData = new AppState({}, events);
 const api = new WebLarekAPI(CDN_URL, API_URL);
 
 const page = new Page(document.body, events);
+const modal = new Modal(ensureElement<HTMLElement>('#modal-container'), events);
 
 const cardCatalogTemplate = ensureElement<HTMLTemplateElement>('#card-catalog');
+const cardPreviewTemplate = ensureElement<HTMLTemplateElement>('#card-preview');
 
 export type CatalogChangeEvent = {
 	catalog: IProduct[];
@@ -32,6 +35,14 @@ api
 		console.error(err);
 	});
 
+events.on('modal:open', () => {
+  page.locked = true;
+});
+
+events.on('modal:close', () => {
+  page.locked = false;
+});  
+
 events.on<CatalogChangeEvent>('larek:changed', () => {
 	page.catalog = appData.catalog.map((item) => {
 		const card = new Card(cloneTemplate(cardCatalogTemplate), {
@@ -43,5 +54,31 @@ events.on<CatalogChangeEvent>('larek:changed', () => {
 			price: item.price,
 			category: item.category,
 		});
+	});
+});
+
+events.on('card:select', (item: IProduct) => {
+	appData.setPreview(item);
+});
+
+events.on('preview:changed', (item: IProduct) => {
+	const card = new Card(cloneTemplate(cardPreviewTemplate), {
+		onClick: () => {
+			events.emit('product:selected', item);
+			card.titleButton =
+				appData.basket.indexOf(item) === -1 ? 'Купить' : 'Удалить из корзины';
+		},
+	});
+
+	modal.render({
+		content: card.render({
+			description: item.description,
+			image: item.image,
+			title: item.title,
+			category: item.category,
+			price: item.price,
+			titleButton:
+				appData.basket.indexOf(item) === -1 ? 'Купить' : 'Удалить из корзины',
+		}),
 	});
 });
